db253ff779ba33035bd57c4ea6793772d1ed40b3
sahil <sahilverma1931@gmail.com>
2025-02-27 17:18:35 +0530

    add option to close modal

--- src/components/editForm/editForm.jsx
+++ src/components/editForm/editForm.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import TaskForm from '../taskForm/taskForm'
 import './editForm.css'
 
-const editForm = ({description, title}) => {
-  return (
-    <div className='edit_form_modal'>
-        <TaskForm desc={description} title={title} button_action="edit"/>
-    </div>
-  )
+const editForm = ({ description, title }) => {
+    const exitModalHandler = () => {
+
+    }
+
+    return (
+        <div className='edit_form_modal'>
+            <div
+                className='modal_exit'
+                onClick={exitModalHandler}>
+                <i className="fa-solid fa-xmark"></i>
+            </div>
+            <TaskForm desc={description} title={title} button_action="edit" />
+        </div>
+    )
 }
 
 export default editForm
--- src/components/taskEdit/taskEdit.jsx
+++ src/components/taskEdit/taskEdit.jsx
@@ -1,19 +1,11 @@
 import React from 'react'
 import '../taskEdit/taskEdit.css'
-// import TaskForm from '../taskForm/taskForm'
 import EditForm from '../editForm/editForm'
 
-
-
-const taskEdit = ({ setShowModal, title, descrition }) => {
-    const clickHandler = () => {
-        setShowModal(false);
-    }
-
+const taskEdit = () => {
     return (
-        <div className='edit_modal' onClick={clickHandler}>
-            {/* <TaskForm /> */}
-            <EditForm title={title} description={descrition}/>
+        <div className='edit_modal' >
+            <EditForm />
         </div>
     )
 }

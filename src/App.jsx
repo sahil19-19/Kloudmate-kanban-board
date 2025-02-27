9969275a3e6a0ccd83339de8d2e5e3542d0a74ac
sahil <sahilverma1931@gmail.com>
2025-02-27 12:54:26 +0530

    using Context API to pass data

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
-import { useState } from "react";
-// import reactLogo from './assets/react.svg'
+import { useState} from "react";
 import "./App.css";
 // import Filter from "./components/filter/filter";
 import Column from "./components/column/column";
 import TaskForm from "./components/taskForm/taskForm";
+import {AppContext} from "./components/contexts/appContext";
+import NewTask from "./components/newTask/newTask";
 
 function App() {
 	const [tasks, setTasks] = useState([]);
-    return (
-		<>
-			<section className="filter-container">
-				<TaskForm />
-			</section>
 
-			<section className="column-container">
-				<Column title="To Do"/>
-				<Column title="In Progress"/>
-				<Column title="Done"/>
-			</section>
+	console.log(tasks);
+
+	return (
+		<>
+			<AppContext.Provider value={{tasks, setTasks}}>
+				<section className="filter-container">
+					{/* <TaskForm /> */}
+					<NewTask/>
+				</section>
+
+				<section className="column-container">
+					<Column title="To Do" status="todo" />
+					<Column title="In Progress" status="in_progress" />
+					<Column title="Done" status="done" />
+				</section>
+			</AppContext.Provider>
 		</>
 	)
 }

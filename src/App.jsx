7d885039445a79fef1be842e9fe7b6eecc3c3338
Sahil <sahilverma1931@gmail.com>
2025-02-28 13:56:27 +0530

    add persistence using localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,37 @@
-import { useState} from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 // import Filter from "./components/filter/filter";
 import Column from "./components/column/column";
-import TaskForm from "./components/taskForm/taskForm";
-import {AppContext} from "./components/contexts/appContext";
+import { AppContext } from "./components/contexts/appContext";
 import NewTask from "./components/newTask/newTask";
 
+const prevTasks = localStorage.getItem("tasks");
+
 function App() {
-	const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState(JSON.parse(prevTasks) || []);
 
-	// console.log(tasks);
+    // console.log(tasks);
+	
+    useEffect(() => {
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }, [tasks]);
 
-	return (
-		<>
-			<AppContext.Provider value={{tasks, setTasks}}>
-				<section className="filter-container">
-					{/* <TaskForm /> */}
-					<NewTask/>
-				</section>
+    return (
+        <>
+            <AppContext.Provider value={{ tasks, setTasks }}>
+                <section className="filter-container">
+                    {/* <TaskForm /> */}
+                    <NewTask />
+                </section>
 
-				<section className="column-container">
-					<Column title="To Do" status="todo" />
-					<Column title="In Progress" status="in_progress" />
-					<Column title="Done" status="done" />
-				</section>
-			</AppContext.Provider>
-		</>
-	)
+                <section className="column-container">
+                    <Column title="To Do" status="todo" />
+                    <Column title="In Progress" status="in_progress" />
+                    <Column title="Done" status="done" />
+                </section>
+            </AppContext.Provider>
+        </>
+    );
 }
 
 export default App;

6f8446e15e161395d36f0dd31bae0e7bdab41af6
Sahil <sahilverma1931@gmail.com>
2025-02-28 17:56:36 +0530

    add DropZone after each TaskCard and at the beginning

--- src/components/column/column.jsx
+++ src/components/column/column.jsx
@@ -1,29 +1,34 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 
-import '../column/column.css'
-import TaskCard from '../taskCard/taskCard';
-import {AppContext} from "../contexts/appContext";
+import "../column/column.css";
+import TaskCard from "../taskCard/taskCard";
+import { AppContext } from "../contexts/appContext";
+import DropZone from "../dropZone/dropZone";
 
-const Column = ({title, status}) => {
-    const {tasks} = useContext(AppContext);
+const Column = ({ title, status }) => {
+    const { tasks } = useContext(AppContext);
     return (
-        <div className='column'>
-            <p className='column-heading'>{title}</p>
+        <div className="column">
+            <p className="column-heading">{title}</p>
+            <DropZone />
             <div>
-                {
-                    tasks.map((task, index) => 
+                {tasks.map(
+                    (task, index) =>
                         task.enteredStatus == status && (
-                            <TaskCard 
-                                title={task.enteredTitle}
-                                description = {task.enteredDescription}
-                                status={task.enteredStatus}
-                                taskIndex={index}
-                            />
-                        ))
-                }
+                            <>
+                                <TaskCard
+                                    title={task.enteredTitle}
+                                    description={task.enteredDescription}
+                                    status={task.enteredStatus}
+                                    taskIndex={index}
+                                />
+                                <DropZone status={status} index={index+1}/>
+                            </>
+                        )
+                )}
             </div>
         </div>
     );
-}
+};
 
 export default Column;
